9ececc99108496fc21e98723cda2b342271352ec
Wiliam Roza <devfrontwill@gmail.com>
2022-09-22 23:01:52 -0300

    Terminado a logica do app, faltando ajustar detalhes porém ja está funcional e pronto para uso

--- app/validacao.js
+++ app/validacao.js
@@ -17,6 +17,8 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
@@ -27,7 +29,6 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
- 
 }
 
 function chuteForInvalido(numero) {
@@ -37,3 +38,9 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorQueOValorPermitido(numero){
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
