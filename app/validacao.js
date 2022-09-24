07123a7a11af59a32c061b889e045bdee6dc2b9d
Wiliam Roza <devfrontwill@gmail.com>
2022-09-24 11:56:47 -0300

    Criado mais uma funcionalidade para quando quisermos finalizar o jogo com a palavra GAME OVER o jogo encerra e vai para uma outra pagina com a opcao de reinicar a partida

--- app/validacao.js
+++ app/validacao.js
@@ -2,9 +2,21 @@ function verificaSeOChutePossuiUmValorValido(chute) {
     const numero = +chute;
 
     if (chuteForInvalido(numero)) {
-        elementoChute.innerHTML += '<div>Valor inválido</div>'
-        return
+        if (chute.toUpperCase() === "GAME OVER") {
+
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
+
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
     }
+
 
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
         elementoChute.innerHTML += `
@@ -29,6 +41,8 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
+
+    
 }
 
 function chuteForInvalido(numero) {
